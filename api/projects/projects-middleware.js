e10e0072a5fe2fe8d1dda3d5a914fe848d6da20b
Aliceson Phillips <alicesonphillips@gmail.com>
2023-07-05 11:26:31 -0400

    fixed everything; 1 error in put for projects

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,62 @@
+const Project = require('./projects-model')
+
 // add middlewares here related to projects
+function logger(req, res, next) {
+    console.log(
+      `[${new Date().toISOString()}] ${req.method} to ${req.url} from ${req.get('Origin')}` 
+    )
+    next()
+  }
+
+function valId (req, res, next) {
+    const { id } = req.params
+
+    Project.get(id)
+    .then(item => {
+        if(item) {
+        req.item = item
+        next()
+        } else {
+            res.status(404).json({ message: "ID not found" })
+        }
+    }) 
+    .catch(err => {
+        res.status(500).json({ message: 'Project not found' })
+
+    })
+}
+
+
+function verifyInfo (req, res, next) {
+   try {
+    const { name, description, completed } = req.body
+
+    if(!name) {
+        res.status(400).json({ message: 'Missing required field: name' })
+    } 
+    if(!description) {
+      res.status(400).json({ message: 'Missing required field: description' })
+    }
+   if(!completed) {
+    res.status(400).json({ message: 'Missing required field: completed' })
+    } 
+    
+    next()
+    
+   }
+   catch(err) {
+    res.status(500).json({ 
+        message: 'Error',
+        err: err.message,
+        stack: err.stack,
+      })
+   }
+}
+
+  module.exports = {
+    logger, 
+    valId,
+    // verifyMoreInfo, 
+    // requiredBody,
+    verifyInfo
+} 
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,1 +1,79 @@
 // Write your "projects" router here!
+const express = require('express')
+
+const router = express.Router()
+
+const projects = require('./projects-model')
+const { verifyMoreInfo, verifyInfo, requiredBody, valId } = require('./projects-middleware')
+
+router.get('/', (req, res) => {
+    
+    projects.get()
+    .then(item => {
+        res.status(200).json(item)
+    })
+    .catch(err => {
+        err: err.message,
+        res.status(500).message(`[]`)
+    })
+})
+
+
+
+router.get('/:id', valId, (req, res) => {
+    res.status(200).json(req.item)
+})
+
+router.post('/', verifyInfo, (req, res) => {
+    const Project = req.body
+    projects.insert(Project)
+    .then(Project => {
+        res.status(201).json(Project)
+    })
+    .catch(err => {
+        res.status(500).json({
+            message: err.message,
+    })
+    })
+
+ })
+
+router.put('/:id', valId, verifyInfo, async (req, res) => {
+    try {
+        const updatedProject = await projects.update(req.params.id, req.body)
+        res.status(200).json(updatedProject)
+    }
+   catch(err){ 
+        console.log(err)
+   }
+})
+
+router.delete('/:id', valId, (req, res) => {
+    projects.remove(req.params.id)
+    .then(count => {
+        if(count > 0) {
+            res.status(200).json({ message: `Project ${req.params.id} has been deleted`})
+        } else {
+            res.status(404).json({ message: `Project ${req.params.id} was not found`})
+        }
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json({
+            message: 'Error'
+        })
+    })
+    
+})
+
+router.get('/:id/actions', valId, (req, res) => {
+    projects.getProjectActions(req.params.id)
+    .then(action => {
+        res.status(200).json(action)
+    })
+    .catch(err => {
+        res.status(500).json('[]')
+    })
+})
+
+module.exports = router
